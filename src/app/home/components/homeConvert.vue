f94130a6fb57bf86ae154d64dca939dbae2f0cb1
Dan1707 <danuhae@gmail.com>
2025-05-11 17:05:59 +0300

    added computed and async function converting

--- src/app/home/components/homeConvert.vue
+++ src/app/home/components/homeConvert.vue
@@ -9,6 +9,10 @@ import { ref } from 'vue'
 const optionCode = ref('')
 const convertedCode = ref('')
 
+const copyConvertedCode = () => {
+	navigator.clipboard.writeText(convertedCode.value || '')
+}
+
 const convertData = (lines: string[]) => {
 	let data = ''
 
@@ -71,21 +75,104 @@ const convertFunctions = (lines: string[]) => {
 			i++
 			while (lines[i] !== '}') {
 				const line = lines[i]
+				// CONDITIONS
+				const includesBrackets =
+					line?.includes('(') ||
+					line?.includes(')') ||
+					line?.includes('{') ||
+					line?.includes('}')
+
+				const includesOthers =
+					line?.includes('if(') ||
+					line?.includes('switch(') ||
+					line?.includes('for(') ||
+					line?.includes('console.log') ||
+					line?.includes('return') ||
+					line?.includes('},')
+
+				let funcObj = {
+					funcFirstLine: '',
+					funcName: '',
+					funcParams: '',
+					funcContent: '',
+				}
+
+				if (includesBrackets && !includesOthers) {
+					const nameMatch = line.match(
+						/^\s*(async\s+)?([\w$]+)\s*\(([^)]*)\)\s*{/
+					)
+
+					const isAsync = nameMatch?.[1]?.includes('async') ?? false
+
+					if (nameMatch) {
+						funcObj.funcName = nameMatch[2] || 'unknown'
+						funcObj.funcParams = nameMatch[3]?.trim() || ''
+					}
+
+					let funcLines: string[] = []
+					let braceCount = 0
+
+					i++
+
+					do {
+						const currentLine = lines[i]
+						funcLines.push(currentLine)
+
+						braceCount += (currentLine.match(/{/g) || []).length
+						braceCount -= (currentLine.match(/}/g) || []).length
+
+						i++
+					} while (i < lines.length && braceCount > 0)
+
+					funcObj.funcContent = funcLines.join('\n')
+
+					if (isAsync) {
+						data += `\nconst ${funcObj.funcName.trim()} = async(${
+							funcObj.funcParams
+						}) => {\n${funcObj.funcContent}
+}
+					`
+					} else {
+						data += `\nconst ${funcObj.funcName.trim()} = (${
+							funcObj.funcParams
+						}) => {\n${funcObj.funcContent}
+}
+					`
+					}
+				}
+
+				i++
+			}
+		}
+	}
+
+	convertedCode.value += data
+}
+
+const convertComputed = (lines: string[]) => {
+	let data = ''
+	for (let i = 0; i < lines.length; i++) {
+		const line = lines[i]
+
+		if (line.startsWith('computed:')) {
+			i++
+			while (lines[i] !== '}') {
+				const line = lines[i]
 
 				// CONDITIONS
 				const includesBrackets =
-					line.includes('(') ||
-					line.includes(')') ||
-					line.includes('{') ||
-					line.includes('}')
+					line?.includes('(') ||
+					line?.includes(')') ||
+					line?.includes('{') ||
+					line?.includes('}')
 
 				const includesOthers =
-					line.includes('if(') ||
-					line.includes('switch(') ||
-					line.includes('for(') ||
-					line.includes('console.log') ||
-					line.includes('return') ||
-					line.includes('},')
+					line?.includes('if(') ||
+					line?.includes('switch(') ||
+					line?.includes('for(') ||
+					line?.includes('console.log') ||
+					line?.includes('return') ||
+					line?.includes('},')
 
 				let funcObj = {
 					funcName: '',
@@ -118,10 +205,10 @@ const convertFunctions = (lines: string[]) => {
 
 					funcObj.funcContent = funcLines.join('\n')
 
-					data += `\nconst ${funcObj.funcName.trim()} = (${
+					data += `\nconst ${funcObj.funcName.trim()} = computed((${
 						funcObj.funcParams
-					}) => {\n${funcObj.funcContent}
-}
+					}) => {\n	${funcObj.funcContent}
+})
 					`
 				}
 
@@ -133,11 +220,35 @@ const convertFunctions = (lines: string[]) => {
 	convertedCode.value += data
 }
 
+const findThis = (lines: string[]) => {
+	for (let i = 0; i < lines.length; i++) {
+		const line = lines[i]
+
+		if (line.startsWith('<script>')) {
+			i++
+			while (lines[i] !== '<' + '/script>') {
+				let line = lines[i]
+
+				if (line?.includes('this.')) {
+					return (line = line.replace(
+						/this\.(\w+)/g,
+						(_, name) => `${name}.value`
+					))
+				}
+				i++
+			}
+		}
+	}
+}
+
 const covertCode = (optionCode: string) => {
 	const lines = optionCode.split('\n').map(line => line.trim())
 
 	convertData(lines)
 	convertFunctions(lines)
+	convertComputed(lines)
+	findThis(lines)
+	copyConvertedCode()
 }
 </script>
 
@@ -161,7 +272,7 @@ const covertCode = (optionCode: string) => {
 				<div class="basis-[450px] min-h-[300px] relative">
 					<div class="flex w-full justify-between absolute p-5">
 						<Label>Composition API</Label>
-						<Button>
+						<Button @click="copyConvertedCode()">
 							<Copy />
 						</Button>
 					</div>
